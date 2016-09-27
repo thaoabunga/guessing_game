ba4a63334ff4144eb3999e7e4cb4e93aa5bd554a
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-27 15:01:03 -0700

    Added error message when out of range

--- game.py
+++ game.py
@@ -10,18 +10,19 @@ chosen_number = random.randint(1,100)
 while True:
 	
 	guess = int(input("Enter a number: "))
-
+    if guess < 1 or guess > 100:
+    	print "You Silly Goose! Enter a valid number from the range 1 to 100"
 	
 	if chosen_number == guess:
 		
 		print "Congratulations, you guessed the correct number!"
 		break
 
-	elif guess>chosen_number:
-		print "Guess a lower number than %d" %guess
+	elif guess > chosen_number:
+		print "Guess a lower number than %d" % guess
 
 	else:
-		print "Guess a higher number than %d." %guess
+		print "Guess a higher number than %d." % guess
 
 
 
