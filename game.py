928fc7df76fd18330d552d1724660462de6d6378
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-27 16:19:02 -0700

    completed guessing game

--- game.py
+++ game.py
@@ -1,29 +1,49 @@
 # Put your code here
 import random
 
-print "Hello, welcome to our number guessing game!"
-print "What's your name?" 
-name = raw_input("----->")
+def game():
 
-chosen_number = random.randint(1,100)
+    print "Hello, welcome to our number guessing game!"
+    print "What's your name?" 
+    name = raw_input("----->")
 
-while True:
-	
-	guess = int(input("Enter a number: "))
-    if guess < 1 or guess > 100:
-    	print "You Silly Goose! Enter a valid number from the range 1 to 100"
-	
-	if chosen_number == guess:
-		
-		print "Congratulations, you guessed the correct number!"
-		break
+    chosen_number = random.randint(1,100)
+    print chosen_number
+    count = 0
+    while True:
 
-	elif guess > chosen_number:
-		print "Guess a lower number than %d" % guess
+  
+        try:
+            guess = int(input("Enter a number: "))
+        except NameError:
+            print "You Silly Goose! Enter a valid number from the range 1 to 100"
+            continue
 
-	else:
-		print "Guess a higher number than %d." % guess
+        if guess < 1 or guess > 100:
+            print "You Silly Goose! Enter a valid number from the range 1 to 100."
+            continue
 
+        elif chosen_number == guess:
+            count = count + 1
 
+            print "Congratulations, you guessed the correct number! You've guessed in %d tries!" % count
 
+            print "Do you want to play another game? yes/no: "
+            response = raw_input(">>>>>" )
+            if response.lower() == "yes":
+                game()
+            elif response.lower() == "no":
+                break
+            else:
+                print "Not a valid response, you are now exiting the game."    
+                break
+        elif guess > chosen_number:
+            count = count + 1
+            print "Guess a lower number than %d" % guess
+            print count
 
+        else:
+            count = count + 1
+            print "Guess a higher number than %d." % guess
+            print count
+game()
